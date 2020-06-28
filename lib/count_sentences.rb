8cd77f91c4f0fd7318dd03c8fa66e0f9cfc8b90b
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-28 17:45:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,36 +1,75 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    if self.end_with? "."
-      true
-    else
-      false
-    end
+    self.end_with?(".")
   end
-
+  
   def question?
-    if self.end_with? "?"
-      true
-    else
-      false 
-    end
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-    if self.end_with? "!"
-      true
-    else
-      false
-    end
+    self.end_with?("!")
   end
-
+  
   def count_sentences
-  self.split(/[.?!]+/).count
-  # self.split.count
+    self.split(/[.?!]+/).count
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class String
+
+#   def sentence?
+#     if self.end_with? "."
+#       true
+#     else
+#       false
+#     end
+#   end
+
+#   def question?
+#     if self.end_with? "?"
+#       true
+#     else
+#       false 
+#     end
+#   end
+
+#   def exclamation?
+#     if self.end_with? "!"
+#       true
+#     else
+#       false
+#     end
+#   end
+
+#   def count_sentences
+#   self.split(/[.?!]+/).count
+#   # self.split.count
+#   end
+# end
 
 
 
